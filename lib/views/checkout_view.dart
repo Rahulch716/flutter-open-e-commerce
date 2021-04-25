188f21e0ed9bf5ed5e42fcb5ae5b42c8990f1df5
unknown <rahulchoudhury716@gmail.com>
2021-04-25 14:16:38 +0530

    Checkout added

--- lib/views/checkout_view.dart
+++ lib/views/checkout_view.dart
@@ -1,39 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:opencommerce/models/models.dart';
+import 'package:opencommerce/services/cart_service.dart';
 
 class CheckoutView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          child: ListView(
-            children: [
-              Card(
-                child: Column(
+        child: StreamBuilder(
+            stream: CartService().getProductStream(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData &&
+                  snapshot.connectionState != ConnectionState.waiting) {
+                final List<Product> products = snapshot.data;
+                return ListView(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Product 1",
-                          style: TextStyle(fontSize: 22),
-                        ),
-                        Image.asset(
-                          'images/mobile.png',
-                          scale: 8,
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [Text('Price 5446')],
-                    ),
+                    ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: products.length,
+                        itemBuilder: (buildContext, int index) {
+                          Product product = products[index];
+
+                          return Card(margin: EdgeInsets.all(6),
+                            color: Colors.green[200],
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            child: Container(padding: EdgeInsets.all(10),
+                              child: Column(
+                                children: [
+                                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:CrossAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        product.name,
+                                        style: TextStyle(fontSize: 22),
+                                      ),
+                                      SizedBox(width: 30,),
+                                      ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(10),
+                                        child: Image.network(
+                                          product.imageUrl,scale: 8,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "₹${product.price}",
+                                        style: TextStyle(fontSize: 20),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
                   ],
-                ),
-              )
-            ],
-          ),
-        ),
+                );
+              } else {
+                return CircularProgressIndicator();
+              }
+            }),
       ),
     );
   }
